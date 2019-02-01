04600c69075e112bcb88f0ecb8d7ad885011379c
Richard Tom <tomrichard14@gmail.com>
2019-01-31 16:44:15 -0800

    changed some styling

--- src/components/Thumbnail.js
+++ src/components/Thumbnail.js
@@ -7,15 +7,15 @@ export class Thumbnail extends Component {
     let thumbnailJSX = thumbnails.map((thumbnail,i) => (
       <li key={`thumbnail-${i}`}>
         <div className="thumb" onTouchMove={e => activeSwitch(e)}>
-          <img src={thumbnail.gif} alt="gif" width="480" height="300" frameBorder="0" allowFullScreen className="gif"/>
-          <img src={thumbnail.image} alt="still" className={`still thumb-${i+1}`} width="480" height="300" frameBorder="0" style={{opacity: active === i+1 ? 0 : 1}}/>
+          <img src={thumbnail.gif} alt="gif" className="gif"/>
+          <img src={thumbnail.image} alt="still" className={`still thumb-${i+1}`} style={{opacity: active === i+1 ? 0 : 1}}/>
         </div>
       </li>
       
     ))
     return (
       <div className="container">
-        <ul >
+        <ul>
           {thumbnailJSX}
         </ul>
       </div>
